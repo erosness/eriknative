e3d4a382f0a919a34df5c0c63c5a7d7a4af5c322
Erik Rosness <erik.rosness@tandbergradio.com>
2019-10-20 13:35:38 +0200

    Minimal changes to test toolchain.

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>Welcome to React Native party at Erik!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
@@ -32,7 +32,7 @@ const styles = StyleSheet.create({
   },
   instructions: {
     textAlign: 'center',
-    color: '#333333',
+    color: '#833333',
     marginBottom: 5,
   },
 });
