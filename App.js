6865ee99fbcfa2a4a6c7acd1090abf3f7b487a79
Erik Rosness <erik.rosness@tandbergradio.com>
2019-10-22 08:23:26 +0200

    Remove content from top-level view.

--- App.js
+++ App.js
@@ -11,31 +11,7 @@ const instructions = Platform.select({
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native party at Erik!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
         <TestFrame1 />
-      </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#833333',
-    marginBottom: 5,
-  },
-});
