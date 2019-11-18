d8ba3ab8174b739807dfd90a7c3f3eb4c4a18422
Erik Rosness <erik@rosness.no>
2019-11-18 13:50:56 +0100

    MOve files around a bit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+import React
 import { AppRegistry, View  } from 'react-native';
-import App from './App';
+import App from './components/App';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
