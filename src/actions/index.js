3aa94abcc11ba26d27764baafc83712ad9a941b9
Erik Rosness <erik@rosness.no>
2019-11-20 11:07:59 +0100

    Rearranged actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,10 @@
-export const newProspects = serverList => ({
-  type: 'NEW_PROSPECTS',
-  serverList
-})
+import { NEW_IP_PORT_PAIR } from "./actionTypes";
+
+export const newIpPortPair = (serverList) =>
+  {
+    console.log("At action!");
+    return {
+      type: NEW_IP_PORT_PAIR,
+      serverList
+    }
+};
