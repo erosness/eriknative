b68960d6fcdfd7b57dafe13be4ddf195670da7c7
Erik Rosness <erik@rosness.no>
2019-11-20 16:53:53 +0100

    Remove debug printout

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,6 @@ import { NEW_IP_PORT_PAIR } from "./actionTypes";
 
 export const newIpPortPair = (serverList) =>
   {
-    console.log("At action!");
     return {
       type: NEW_IP_PORT_PAIR,
       serverList
