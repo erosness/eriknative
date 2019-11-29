21b4d0a1e02f10224eaf88c11f7d9332968a8cc0
Erik Rosness <erik@rosness.no>
2019-11-29 08:57:07 +0100

    Cleanup, remove old Rate

--- src/components/TestFrame1.js
+++ src/components/TestFrame1.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 import { smStyles } from './SmFrameStyle';
 
-import {Rate} from './Rate';
 
 class TestFrameX extends React.Component {
   render() {
@@ -24,7 +23,7 @@ class TestFrame1 extends React.Component {
           <TestFrameX style={{flex: 4, backgroundColor: 'powderblue'}} />
         </View>
         <View style={{backgroundColor: 'steelblue'}}>
-          <Rate totalStars={11} initialRate={6} style={{flex: 4, backgroundColor: 'powderblue'}}  />
+          <Text style={{flex: 4, backgroundColor: 'powderblue'}}  />
         </View>
       </View>
     )
