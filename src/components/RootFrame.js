4edeceba0772a9b2b5e53b922692f7d86fd90ae9
Erik Rosness <erik@rosness.no>
2019-11-29 09:32:35 +0100

    More cleanup in styles etc.

--- src/components/RootFrame.js
+++ src/components/RootFrame.js
@@ -6,17 +6,18 @@ class RootFrame extends React.Component {
   render() {
     return (
       <View style={[smStyles.topFrame]}>
-        <View style={{backgroundColor: 'lightsteelblue'}}>
-          <Text style={{textAlign: 'center',
-                        padding: 5,
-                        fontSize: 24}} > SmartMonitor</Text>
-          <Text style={{backgroundColor: 'powderblue',
-                        padding: 5,}}>
-                        The SmartMonitor is a smart monitoring and
-                        contol setup for seamless integration and
-                        operation.
-          </Text >
-        </View>
+        <Text style={{textAlign: 'center',
+                      padding: 5,
+                      fontSize: 24,
+                      backgroundColor: 'lightsteelblue'}} >
+                      SmartMonitor
+        </Text>
+        <Text style={{backgroundColor: 'powderblue',
+                      padding: 5,}}>
+                      The SmartMonitor is a smart monitoring and
+                      contol setup for seamless integration and
+                      operation.
+        </Text >
       </View>
     )
   };
