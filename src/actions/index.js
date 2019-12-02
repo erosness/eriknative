192bd01a41dbb9d09f8d2f1374dfe1ce7c906e21
Erik Rosness <erik@rosness.no>
2019-12-02 15:47:47 +0100

    Add action to fetch function info

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,9 @@ import {
   FETCH_INFO_REQUEST,
   FETCH_INFO_FAILURE,
   FETCH_INFO_SUCCESS,
+  FETCH_DOORBELL_OUT_REQUEST,
+  FETCH_DOORBELL_OUT_FAILURE,
+  FETCH_DOORBELL_OUT_SUCCESS,
 } from "./actionTypes";
 
 export const getInfo = (ipPortPair) =>
@@ -51,6 +54,58 @@ function fetchInfo(ipPortPair) {
       )
       .then(json => {
         dispatch(fetchInfoSuccess(ipPortPair, json))
+      })
+  }
+}
+
+export const getDoorbellOut = (unit) =>
+{
+  console.log("Action getDoorbellOut:", unit)
+  return fetchDoorbellOut(unit)
+};
+
+export const fetchDoorbellOutRequest = (unit) =>
+  {
+    return {
+      type: FETCH_DOORBELL_OUT_REQUEST,
+      payload: {
+        smServer: unit
+    }
+  }
+};
+
+export const fetchDoorbellOutFailure = (unit) =>
+  {
+    return {
+      type: FETCH_DOORBELL_OUT_FAILURE,
+      payload: {
+        smServer: unit
+    }
+  }
+};
+
+export const fetchDoorbellOutSuccess = (unit,data) =>
+  {
+    console.log("Fetch doorbell status success:",data)
+    return {
+      type: FETCH_DOORBELL_OUT_SUCCESS,
+      payload: {
+        smServer: unit,
+        info: data
+    }
+  }
+};
+
+function fetchDoorbellOut(unit) {
+  return function(dispatch) {
+    dispatch(fetchDoorbellOutRequest(unit))
+    return fetch("http://" + unit.ip + ":" + unit.port + "/v1/sm/doorbell-out/status")
+      .then(
+        response => response.json(),
+        error => console.log('An error occurred.', error)
+      )
+      .then(json => {
+        dispatch(fetchDoorbellOutSuccess(unit, json))
       })
   }
 }
