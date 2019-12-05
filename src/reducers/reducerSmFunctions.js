e77050d747ab7a9526f91b24a49dc7318f0a6db2
Erik Rosness <erik@rosness.no>
2019-12-05 09:57:01 +0100

    Remove redundant objects

--- src/reducers/reducerSmFunctions.js
+++ src/reducers/reducerSmFunctions.js
@@ -29,8 +29,6 @@ export default function(state = initialState, action) {
           cap    : e.cap,
           uid    : action.payload.info.uid,
           name   : action.payload.info.name,
-          status : {},
-          local  : {},
         }
         localFuncs[e.fid]= func
       })
