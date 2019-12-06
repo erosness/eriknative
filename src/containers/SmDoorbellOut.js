cba4aaabb7f2ad24e886a5755828ad6542c647c8
Erik Rosness <erik@rosness.no>
2019-12-06 11:59:28 +0100

    Add code developed in Debug class

--- src/containers/SmDoorbellOut.js
+++ src/containers/SmDoorbellOut.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import { FlatList, Text, View, StyleSheet  } from 'react-native';
 import { smStyles } from '../styles/SmFrameStyle';
 import ShowDoorbellOut from '../components/ShowDoorbellOut';
+import { connect } from 'react-redux';
 
-export default class SmDoorbellOut extends React.Component {
+import { getStatus } from '../actions';
+
+class SmDoorbellOut extends React.Component {
   constructor(props){
     super(props);
     this.state ={ isLoading: true}
@@ -14,8 +17,18 @@ export default class SmDoorbellOut extends React.Component {
     return (
       // Try setting `flexDirection` to `column`.
       <View style={[smStyles.topFrame, {flexDirection: 'column'}]}>
-        <ShowDoorbellOut elem={this.props.elem} />
+        <ShowDoorbellOut elem={this.props.elem} functionStatus={this.props.functionStatus}/>
       </View>
     );
   };
 }
+
+
+const mapStateToProps = state => {
+  return {unitList: state.unitList, functionStatus: state.functionStatus};
+};
+
+export default connect(
+  mapStateToProps,
+  { getStatus },
+)(SmDoorbellOut);
