2abc8320bc3bd9567315996207ab8c11db342b85
Erik Rosness <erik@rosness.no>
2020-01-23 12:44:14 +0100

    Worked on reducer. Taking shape now.

--- src/reducers/reducerUIDoorbell1.js
+++ src/reducers/reducerUIDoorbell1.js
@@ -1,25 +1,37 @@
 import {
-  FETCH_INFO_REQUEST,
-  FETCH_INFO_FAILURE,
   FETCH_INFO_SUCCESS,
+  FETCH_STATUS_SUCCESS,
 } from "../actions/actionTypes";
 
 let initialState = {};
 
+const pidDoorbellOut = 814169;
+
 export default function(state = initialState, action) {
   switch (action.type) {
     case FETCH_INFO_SUCCESS: {
-      let localFuncs = Object.assign({}, state.functionList);
+      var o = state;
       action.payload.info.cap.forEach( e => {
-        const func = {
-          fid    : e.fid,
-          cap    : e.cap,
-          uid    : action.payload.info.uid,
-          name   : action.payload.info.name,
+        if(e.fid == pidDoorbellOut) {
+          const func = {
+            fid    : e.fid,
+            cap    : e.cap,
+            uid    : action.payload.info.uid,
+            name   : action.payload.info.name,
+          }
+          o = Object.assign({}, state, {info: func})
+        } else {
+          return state;
         }
-        localFuncs[e.fid]= func
       })
-      return Object.assign({}, state, localFuncs)
+      return o
+    }
+    case FETCH_STATUS_SUCCESS: {
+      var o = state;
+      if(action.payload.status.fid == pidDoorbellOut) {
+        o = Object.assign({}, state, {status: action.payload.status})
+      }
+      return o
     }
     default: {
       return state;
