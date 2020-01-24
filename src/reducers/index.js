758d998098faa0025ec1ad5c85a49641d732e1d3
Erik Rosness <erik@rosness.no>
2020-01-24 12:52:02 +0100

    Add reducer for display

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux'
 import unitList from './reducerSmInfo'
 import functionStatus from './reducerSmStatus'
 import functionUIDoorbell1 from './reducerUIDoorbell1'
+import functionDisplay8x8 from './reducerDisplay8x8'
 import functionList from './reducerSmFunctions'
 
 export default combineReducers({
   unitList,
   functionList,
   functionUIDoorbell1,
+  functionDisplay8x8,
   functionStatus,
 })
