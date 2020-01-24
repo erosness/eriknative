aa057895c0ae615683a051667751c5821862f9f4
Erik Rosness <erik@rosness.no>
2020-01-24 16:33:10 +0100

    Add & rename reduces results

--- src/reducers/reducerUIDoorbell1.js
+++ src/reducers/reducerUIDoorbell1.js
@@ -5,26 +5,34 @@ import {
 
 let initialState = {};
 
-const pidDoorbellOut = 814169;
+const fidDoorbellOut = 814169;
+const fidDoorbellIn = 4040403;
 
 export default function(state = initialState, action) {
   switch (action.type) {
     case FETCH_INFO_SUCCESS: {
       var o = state;
       action.payload.info.cap.forEach( e => {
-        if(e.fid == pidDoorbellOut) {
+        if(e.fid == fidDoorbellOut) {
           const func = action.payload.info
-          o = Object.assign({}, state, {info: func})
-        } else {
-          return state;
+          o = Object.assign({}, state, {infoDoorbellOut: func})
+        }
+      })
+      action.payload.info.cap.forEach( e => {
+        if(e.fid == fidDoorbellIn) {
+          const func = action.payload.info
+          o = Object.assign({}, state, {infoDoorbellIn: func})
         }
       })
       return o
     }
     case FETCH_STATUS_SUCCESS: {
       var o = state;
-      if(action.payload.status.fid == pidDoorbellOut) {
-        o = Object.assign({}, state, {status: action.payload.status})
+      if(action.payload.status.fid == fidDoorbellOut) {
+        o = Object.assign({}, state, {statusDoorbellOut: action.payload.status})
+      }
+      if(action.payload.status.fid == fidDoorbellIn) {
+        o = Object.assign({}, state, {statusDoorbellIn: action.payload.status})
       }
       return o
     }
